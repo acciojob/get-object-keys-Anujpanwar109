2f391db7e3c2702ee84137608206e6a2df5e3fde
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-15 19:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const student = {
+const myObj = {
   name: "John",
+  getKeys: function () {
+    return Object.keys(this);
+  }
 };
-
-
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-
-console.log(getKeys(student)); 
-
-
-const studentWithMoreProperties = {
-  name: "John",
-  age: 25,
-  city: "New York",
-};
-console.log(getKeys(studentWithMoreProperties)); 
-
-
-const emptyObject = {};
-console.log(getKeys(emptyObject)); 
+console.log(myObj.getKeys());
